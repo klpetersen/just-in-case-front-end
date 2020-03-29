6c3ce61d5c42f06a6e233eb5a334d04dea0a0ba8
github username <github email address>
2020-03-29 19:57:52 -0400

    more fixes and such

--- src/components/Question.js
+++ src/components/Question.js
@@ -32,6 +32,11 @@ export default function Question(props) {
           trueRef.className = value === answer && answer ? 'question-button correct-button' : 'question-button disabled-button'
           falseRef.disabled = true;
           falseRef.className =  value === answer && !answer ? 'question-button correct-button' : 'question-button disabled-button'
+          if(value) { 
+            trueRef.className += ' picked-button' 
+          } else { 
+            falseRef.className += ' picked-button' 
+          }
       }
 
       const handleNext = () => {
